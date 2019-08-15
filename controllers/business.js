796153726f43db18e3de1e6c21a6a4c2e4c70f55
Julie Fryer <julie@Julies-MacBook-Pro-2.local>
2019-08-15 17:27:12 -0400

    push

---
+++ controllers/business.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const Townies = require("../models/____.js");
+
+
+///////////////////////ROUTES////////////////////////////
+
+//Create
+router.post("/", (req,res) => {
+  Townies.create(req.body, (error, createdTownie) => {
+      res.json(createdTownie)
+  });
+});
+
+//Index
+router.get("/", (req, res) => {
+
+  });
+});
+
+//Update
+router.put("/:id", (req, res) => {
+
+  });
+});
+
+//Delete
+router.delete("/:id", (req, res) => {
+
+  });
+});
+
+module.exports = router;
