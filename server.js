d80cc799f1b0cef8c6e6027376f7dd28803c032c
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-08-15 18:08:53 -0500

    got server connection working

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
+/***********************************
+ * Dependancies and Configurations *
+ *                                 *
+ ***********************************/
 // console.log(process.env.PORT);
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
-const db = mongoose.connection;
-require('dotenv').config()
-
+require('dotenv').config();
+const app = express();
 
 const PORT = process.env.PORT || 3000;
+const db = mongoose.connection;
 
-// const MONGODB_URI = process.env.MONGODB_URI;
-const MONGODB_URI = "mongodb+srv://Justin:Stitch2019@local-business-duuiv.mongodb.net/test?retryWrites=true&w=majority"
-
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
-
-db.on('connected', () => console.log('mongo connected:'));
-
+/**************
+ * Middleware *
+ *            *
+ **************/
 app.use(express.static('public'));
 app.use(express.json());
 app.use(session({
@@ -25,11 +25,27 @@ app.use(session({
 	saveUninitialized: false
 }));
 
+/************
+ * Database *
+ *          *
+ ************/
+// const MONGODB_URI = process.env.MONGODB_URI;
+const mongodbURI = "mongodb+srv://jwaltz001:Stitch2019@local-business-duuiv.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongodbURI, {useNewUrlParser: true});
+db.on('connected', () => console.log('mongo connected:'));
 
+/***************
+ * Controllers *
+ *             *
+ ***************/
 const userController = require("./controllers/business.js");
 app.use("/reviews", userController);
 
-const sessionController = require("./controllers/sessions.js");
+const sessionController = require("./controllers/sessions-users.js");
 app.use("/sessions", sessionController);
 
+/************
+ * Listener *
+ *          *
+ ************/
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
