dbb162bc37df0e8daae69f4e47b5f25f5092c35f
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-08-15 21:49:09 -0500

    updated models and controllers, New User not working in Postman

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
@@ -40,7 +40,8 @@ app.use("/business", userController);
 
 const sessionController = require("./controllers/sessions-users.js");
 app.use("/sessions", sessionController);
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 /************
  * Listener *
  *          *
