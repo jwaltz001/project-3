d396ec2a34732789c08a325fb73ad09f6ca1c7ef
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-08-20 12:48:49 -0500

    server call

--- public/app.js
+++ public/app.js
@@ -282,8 +282,11 @@ app.controller('AppController', ['$http', function($http){
 	this.searchYelp = () => {
 		$http({
 			method: "GET",
-			url: "https://api.yelp.com/v3/businesses/search?limit=10&term=" + this.yelpSearchName + "&location=" + this.yelpSearchCity + this.yelpSearchState,
-			Authorization: "Bearer nSZt3_3hdEoT09d9VuVMUbkQUz7JIViAKxl_DLMSpwmuMkD6CWPhaOOA62rf5qExfj7q8pDw07FRfxQw3ibkR-PpIAMggNu4manvUY2af0dRP9sBJVU1SCmzSpRYXXYx"
+			url: "/yelp",
+			data:{
+				apiUrl:"https://api.yelp.com/v3/businesses/search?limit=10&term=" + this.yelpSearchName + "&location=" +this.yelpSearchCity + this.yelpSearchState,
+				Authorization: "Bearer nSZt3_3hdEoT09d9VuVMUbkQUz7JIViAKxl_DLMSpwmuMkD6CWPhaOOA62rf5qExfj7q8pDw07FRfxQw3ibkR-PpIAMggNu4manvUY2af0dRP9sBJVU1SCmzSpRYXXYx"
+		}
 		}).then((res) => {
 			console.log("yelp search return",res.data);
 		})
